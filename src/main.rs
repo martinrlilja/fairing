54b735ae4756f8e4d64ce2a8c20a4bdb2646d6fb
Martin Risell Lilja <martin.risell.lilja@gmail.com>
2024-06-13 23:05:58 +0200

    feat: use object_store for files. endpoint for creating sites.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use axum::{
     body::Bytes,
-    extract::{path::Path, DefaultBodyLimit, Query},
+    extract::{path::Path, DefaultBodyLimit, Query, State},
     http::{
         header::{self, HeaderMap},
         StatusCode,
@@ -11,9 +11,11 @@ use axum::{
     Json, Router,
 };
 use clap::{Parser, Subcommand};
+use object_store::ObjectStore;
 use serde::{Deserialize, Serialize};
-use std::{io::ErrorKind, path::PathBuf};
+use std::{path::PathBuf, sync::Arc};
 use tokio::fs;
+use url::Url;
 use uuid::Uuid;
 
 #[derive(Debug, Parser)]
@@ -25,13 +27,23 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Server,
+    Server {
+        object_store_url: Url,
+    },
     #[command(arg_required_else_help = true)]
     Push {
+        #[arg(long)]
+        create_site: bool,
+
         site_name: String,
         remote: String,
         path: PathBuf,
     },
+}
+
+struct AppState {
+    object_store: Box<dyn ObjectStore>,
+    base_path: object_store::path::Path,
 }
 
 #[tokio::main]
@@ -41,21 +53,31 @@ async fn main() -> Result<()> {
     let args = Cli::parse();
 
     match args.command {
-        Commands::Server => {
+        Commands::Server { object_store_url } => {
+            let (object_store, base_path) = object_store::parse_url(&object_store_url)?;
+
+            let app_state = Arc::new(AppState {
+                object_store,
+                base_path,
+            });
+
             let app = Router::new()
                 .route("/sites/:site_name/current/", get(get_file))
                 .route("/sites/:site_name/current/*path", get(get_file))
                 .route("/sites/:site_name/:deployment_id/", get(get_file))
                 .route("/sites/:site_name/:deployment_id/*path", get(get_file))
                 .route("/api/v1/deployments", post(create_deployment))
-                .route("/api/v1/files", post(create_file))
+                .route("/api/v1/files", put(create_file))
+                .route("/api/v1/sites", put(create_site))
                 .route("/api/v1/sites/:site_name", put(update_site))
+                .with_state(app_state)
                 .layer(DefaultBodyLimit::max(256 * 2_usize.pow(20)));
 
             let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
             axum::serve(listener, app).await.unwrap();
         }
         Commands::Push {
+            create_site,
             site_name,
             remote,
             path: base_path,
@@ -106,6 +128,18 @@ async fn main() -> Result<()> {
 
             let client = reqwest::Client::new();
 
+            if create_site {
+                client
+                    .put(format!("{remote}/api/v1/sites"))
+                    .json(&CreateSite {
+                        name: site_name.clone(),
+                    })
+                    .send()
+                    .await?
+                    .error_for_status()
+                    .unwrap();
+            }
+
             let deployment = client
                 .post(format!("{remote}/api/v1/deployments"))
                 .json(&CreateDeployment {
@@ -131,7 +165,7 @@ async fn main() -> Result<()> {
                     .unwrap();
 
                 client
-                    .post(format!("{remote}/api/v1/files"))
+                    .put(format!("{remote}/api/v1/files"))
                     .query(&CreateFileQuery {
                         site_name: site_name.clone(),
                         deployment_id: deployment.id,
@@ -171,30 +205,41 @@ struct GetFilePath {
     path: String,
 }
 
-async fn get_file(Path(path): Path<GetFilePath>) -> impl IntoResponse {
+async fn get_file(
+    State(state): State<Arc<AppState>>,
+    Path(path): Path<GetFilePath>,
+) -> impl IntoResponse {
     let mut headers = HeaderMap::new();
 
     let mut full_path = String::with_capacity(path.path.len() + 1);
     full_path.push_str("/");
     full_path.push_str(&path.path);
 
-    let site_path = {
-        let mut site_path = PathBuf::from(".data/sites");
-        site_path.push(&path.site_name);
-        site_path
-    };
+    let site_path = state
+        .base_path
+        .child("sites")
+        .child(path.site_name.as_str());
+    let site_metadata_path = site_path.child("metadata.json");
 
-    let metadata_path = {
-        let mut metadata_path = site_path.clone();
-        metadata_path.push("metadata.json");
-        metadata_path
-    };
+    let site_metadata: SiteMetadata = match state.object_store.get(&site_metadata_path).await {
+        Ok(site_metadata_obj) => {
+            let site_metadata_raw = site_metadata_obj.bytes().await.unwrap();
+            serde_json::from_slice(&site_metadata_raw).unwrap()
+        }
+        Err(object_store::Error::NotFound { .. }) => {
+            headers.insert(header::CONTENT_TYPE, "text/plain".parse().unwrap());
+            return (StatusCode::NOT_FOUND, headers, b"site not found".to_vec());
+        }
+        Err(err) => {
+            tracing::error!("get_file: metadata {err:?}");
 
-    let site_metadata: SiteMetadata = if fs::try_exists(&metadata_path).await.unwrap() {
-        serde_json::from_slice(&fs::read(&metadata_path).await.unwrap()).unwrap()
-    } else {
-        headers.insert(header::CONTENT_TYPE, "text/plain".parse().unwrap());
-        return (StatusCode::NOT_FOUND, headers, b"site not found".to_vec());
+            headers.insert(header::CONTENT_TYPE, "text/plain".parse().unwrap());
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                headers,
+                b"internal error".to_vec(),
+            );
+        }
     };
 
     let path_hash = blake3::hash(full_path.as_bytes());
@@ -225,13 +270,12 @@ async fn get_file(Path(path): Path<GetFilePath>) -> impl IntoResponse {
         );
     };
 
-    let mut file_path = site_path.clone();
-    file_path.push("files");
-    file_path.push(deployment_id.to_string());
-    file_path.push(&path_hash_hex[..2]);
-    file_path.push(&path_hash_hex[2..]);
-
-    let bytes = fs::read(&file_path).await.unwrap();
+    let file_path = site_path
+        .child("deployments")
+        .child(deployment_id.to_string())
+        .child("files")
+        .child(&path_hash_hex[..2])
+        .child(&path_hash_hex[2..]);
 
     let (_, ext) = path.path.rsplit_once(".").unwrap_or(("", ""));
 
@@ -243,10 +287,14 @@ async fn get_file(Path(path): Path<GetFilePath>) -> impl IntoResponse {
 
     headers.insert(header::CONTENT_TYPE, mime.parse().unwrap());
 
-    (StatusCode::OK, headers, bytes)
+    let file_obj = state.object_store.get(&file_path).await.unwrap();
+    let file_raw = file_obj.bytes().await.unwrap().to_vec();
+
+    (StatusCode::OK, headers, file_raw)
 }
 
 async fn create_deployment(
+    State(state): State<Arc<AppState>>,
     Json(create_deployment): Json<CreateDeployment>,
 ) -> (StatusCode, Json<Deployment>) {
     let deployment = Deployment {
@@ -258,38 +306,49 @@ async fn create_deployment(
             .collect(),
     };
 
-    let mut stage_dir_path = PathBuf::from(".data/sites");
-    stage_dir_path.push(&create_deployment.site_name);
-    stage_dir_path.push("stage");
-
-    fs::create_dir_all(&stage_dir_path).await.unwrap();
-
-    let mut stage_path = stage_dir_path.clone();
-    stage_path.push(format!("{}.json", deployment.id.to_string()));
+    let deployment_path = state
+        .base_path
+        .child("sites")
+        .child(create_deployment.site_name.as_str())
+        .child("deployments")
+        .child(deployment.id.to_string())
+        .child("metadata.json");
 
     let deployment_stage_meta = serde_json::to_vec(&create_deployment).unwrap();
 
-    fs::write(&stage_path, deployment_stage_meta).await.unwrap();
+    state
+        .object_store
+        .put_opts(
+            &deployment_path,
+            deployment_stage_meta.into(),
+            object_store::PutOptions {
+                mode: object_store::PutMode::Create,
+                ..Default::default()
+            },
+        )
+        .await
+        .unwrap();
 
     (StatusCode::CREATED, Json(deployment))
 }
 
-async fn create_file(Query(query): Query<CreateFileQuery>, body: Bytes) -> StatusCode {
-    let site_path = {
-        let mut site_path = PathBuf::from(".data/sites");
-        site_path.push(&query.site_name);
-        site_path
-    };
+async fn create_file(
+    State(state): State<Arc<AppState>>,
+    Query(query): Query<CreateFileQuery>,
+    body: Bytes,
+) -> StatusCode {
+    let deployment_dir_path = state
+        .base_path
+        .child("sites")
+        .child(query.site_name.as_str())
+        .child("deployments")
+        .child(query.deployment_id.to_string());
 
-    let stage_path = {
-        let mut stage_path = site_path.clone();
-        stage_path.push("stage");
-        stage_path.push(format!("{}.json", query.deployment_id.to_string()));
-        stage_path
-    };
+    let deployment_path = deployment_dir_path.child("metadata.json");
 
+    let deployment_stage_meta_obj = state.object_store.get(&deployment_path).await.unwrap();
     let deployment_stage_meta: CreateDeployment =
-        serde_json::from_slice(&fs::read(stage_path).await.unwrap()).unwrap();
+        serde_json::from_slice(&deployment_stage_meta_obj.bytes().await.unwrap()).unwrap();
 
     let Some(file) = deployment_stage_meta
         .files
@@ -308,83 +367,126 @@ async fn create_file(Query(query): Query<CreateFileQuery>, body: Bytes) -> Statu
     let path_hash = blake3::hash(&query.path.as_bytes());
     let path_hash_hex = path_hash.to_hex();
 
-    let file_dir_path = {
-        let mut file_dir_path = site_path.clone();
-        file_dir_path.push("files");
-        file_dir_path.push(query.deployment_id.to_string());
-        file_dir_path.push(&path_hash_hex[..2]);
-        file_dir_path
-    };
+    let file_path = deployment_dir_path
+        .child("files")
+        .child(&path_hash_hex[..2])
+        .child(&path_hash_hex[2..]);
 
-    fs::create_dir_all(&file_dir_path).await.unwrap();
-
-    let file_path = {
-        let mut file_path = file_dir_path.clone();
-        file_path.push(&path_hash_hex[2..]);
-        file_path
-    };
-
-    let tmp_file_path = file_path.with_extension(".tmp");
-
-    fs::write(&tmp_file_path, body).await.unwrap();
-
-    fs::rename(&tmp_file_path, &file_path).await.unwrap();
+    state
+        .object_store
+        .put(&file_path, body.into())
+        .await
+        .unwrap();
 
     StatusCode::CREATED
 }
 
+async fn create_site(
+    State(state): State<Arc<AppState>>,
+    Json(create_site): Json<CreateSite>,
+) -> StatusCode {
+    let site_metadata_path = state
+        .base_path
+        .child("sites")
+        .child(create_site.name.as_str())
+        .child("metadata.json");
+
+    let site_metadata = SiteMetadata {
+        current_deployment_id: None,
+        finalized_deployment_ids: vec![],
+    };
+
+    let site_metadata_raw = serde_json::to_vec(&site_metadata).unwrap();
+
+    let res = state
+        .object_store
+        .put_opts(
+            &site_metadata_path,
+            site_metadata_raw.into(),
+            object_store::PutOptions {
+                mode: object_store::PutMode::Create,
+                ..Default::default()
+            },
+        )
+        .await;
+
+    match res {
+        Ok(_) => StatusCode::CREATED,
+        Err(object_store::Error::AlreadyExists { .. }) => StatusCode::OK,
+        Err(err) => {
+            tracing::error!("create_site: {err:?}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
 async fn update_site(
+    State(state): State<Arc<AppState>>,
     Path(site_name): Path<String>,
     Json(update_site): Json<UpdateSite>,
 ) -> StatusCode {
-    let site_path = {
-        let mut site_path = PathBuf::from(".data/sites");
-        site_path.push(&site_name);
-        site_path
-    };
+    let site_path = state.base_path.child("sites").child(site_name.as_str());
+    let site_metadata_path = site_path.child("metadata.json");
 
-    let metadata_path = {
-        let mut metadata_path = site_path.clone();
-        metadata_path.push("metadata.json");
-        metadata_path
-    };
+    let (mut site_metadata, site_metadata_obj_meta) =
+        match state.object_store.get(&site_metadata_path).await {
+            Ok(site_metadata_obj) => {
+                let site_metadata_obj_meta = object_store::UpdateVersion {
+                    e_tag: site_metadata_obj.meta.e_tag.clone(),
+                    version: site_metadata_obj.meta.version.clone(),
+                };
 
-    let mut site_metadata = match fs::read(&metadata_path).await {
-        Ok(metadata_raw) => serde_json::from_slice(&metadata_raw).unwrap(),
-        Err(err) if err.kind() == ErrorKind::NotFound => SiteMetadata {
-            current_deployment_id: None,
-            finalized_deployment_ids: vec![],
-        },
-        Err(err) => panic!("{err:?}"),
-    };
+                let site_metadata_raw = site_metadata_obj.bytes().await.unwrap();
+                let site_metadata: SiteMetadata =
+                    serde_json::from_slice(&site_metadata_raw).unwrap();
 
-    if let Some(finalize_deployment_id) = update_site.finalize_deployment_id {
-        let stage_path = {
-            let mut stage_path = site_path.clone();
-            stage_path.push("stage");
-            stage_path.push(format!("{}.json", finalize_deployment_id.to_string()));
-            stage_path
+                (site_metadata, site_metadata_obj_meta)
+            }
+            Err(object_store::Error::NotFound { .. }) => {
+                return StatusCode::NOT_FOUND;
+            }
+            Err(err) => {
+                tracing::error!("get_file: metadata {err:?}");
+                return StatusCode::INTERNAL_SERVER_ERROR;
+            }
         };
 
-        let deployment_stage_meta: CreateDeployment =
-            serde_json::from_slice(&fs::read(stage_path).await.unwrap()).unwrap();
+    if let Some(finalize_deployment_id) = update_site.finalize_deployment_id {
+        let deployment_path = site_path
+            .child("deployments")
+            .child(finalize_deployment_id.to_string());
+        let deployment_metadata_path = deployment_path.child("metadata.json");
 
-        for file in deployment_stage_meta.files.iter() {
+        let deployment_metadata_obj = state
+            .object_store
+            .get(&deployment_metadata_path)
+            .await
+            .unwrap();
+
+        let deployment_metadata = deployment_metadata_obj.bytes().await.unwrap();
+
+        let deployment_metadata: CreateDeployment =
+            serde_json::from_slice(&deployment_metadata).unwrap();
+
+        for file in deployment_metadata.files.iter() {
             let path_hash = blake3::hash(&file.path.as_bytes());
             let path_hash_hex = path_hash.to_hex();
 
-            let file_path = {
-                let mut file_path = site_path.clone();
-                file_path.push("files");
-                file_path.push(finalize_deployment_id.to_string());
-                file_path.push(&path_hash_hex[..2]);
-                file_path.push(&path_hash_hex[2..]);
-                file_path
-            };
+            let file_path = deployment_path
+                .child("files")
+                .child(&path_hash_hex[..2])
+                .child(&path_hash_hex[2..]);
 
-            if !fs::try_exists(&file_path).await.unwrap() {
-                return StatusCode::BAD_REQUEST;
-            }
+            match state.object_store.head(&file_path).await {
+                Ok(_) => (),
+                Err(object_store::Error::NotFound { .. }) => {
+                    return StatusCode::BAD_REQUEST;
+                }
+                Err(err) => {
+                    tracing::error!("get_file: metadata {err:?}");
+                    return StatusCode::INTERNAL_SERVER_ERROR;
+                }
+            };
         }
 
         site_metadata
@@ -405,7 +507,19 @@ async fn update_site(
     }
 
     let site_metadata_raw = serde_json::to_vec(&site_metadata).unwrap();
-    fs::write(&metadata_path, &site_metadata_raw).await.unwrap();
+
+    state
+        .object_store
+        .put_opts(
+            &site_metadata_path,
+            site_metadata_raw.into(),
+            object_store::PutOptions {
+                mode: object_store::PutMode::Update(site_metadata_obj_meta),
+                ..Default::default()
+            },
+        )
+        .await
+        .unwrap();
 
     StatusCode::OK
 }
@@ -442,6 +556,11 @@ struct CreateFileQuery {
 struct SiteMetadata {
     current_deployment_id: Option<Uuid>,
     finalized_deployment_ids: Vec<Uuid>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct CreateSite {
+    name: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
