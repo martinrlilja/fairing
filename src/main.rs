c383899cd5f22328f937a127d02255f74a0bb01b
Martin Risell Lilja <martin.risell.lilja@gmail.com>
2024-06-15 17:07:33 +0200

    feat: file metadata, gzip, parallel uploads.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ use axum::{
     body::Bytes,
     extract::{path::Path, DefaultBodyLimit, Query, State},
     http::{
-        header::{self, HeaderMap},
+        header::{self, HeaderMap, HeaderName},
         StatusCode,
     },
     response::IntoResponse,
     routing::{get, post, put},
     Json, Router,
 };
+use bincode::Options;
 use clap::{Parser, Subcommand};
 use object_store::ObjectStore;
 use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    io::{Cursor, Read},
+    path::PathBuf,
+    sync::Arc,
+};
 use tokio::fs;
 use url::Url;
 use uuid::Uuid;
@@ -106,18 +111,113 @@ async fn main() -> Result<()> {
                     }
 
                     if file_type.is_file() {
-                        let data = fs::read(entry.path()).await?;
-
-                        let hash = blake3::hash(&data);
-
                         let entry_path = entry.path();
                         let mut relative_path =
                             entry_path.strip_prefix(&base_path).unwrap().to_owned();
+
+                        let extension = relative_path
+                            .extension()
+                            .and_then(|ext| ext.to_str())
+                            .unwrap_or("");
+
+                        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+                        const TEXT: &str = "text";
+                        const IMAGE: &str = "image";
+                        const FONT: &str = "font";
+                        const APPLICATION: &str = "application";
+                        let content_type = match extension {
+                            // text
+                            "html" | "htm" => (TEXT, "html"),
+                            "css" => (TEXT, "css"),
+                            "csv" => (TEXT, "csv"),
+                            "ics" => (TEXT, "calendar"),
+                            "md" => (TEXT, "markdown"),
+                            // image
+                            "apng" => (IMAGE, "apng"),
+                            "avif" => (IMAGE, "avif"),
+                            "bmp" => (IMAGE, "bmp"),
+                            "gif" => (IMAGE, "gif"),
+                            "ico" => (IMAGE, "vnd.microsoft.icon"),
+                            "jpeg" | "jpg" => (IMAGE, "jpeg"),
+                            "svg" => (IMAGE, "svg+xml"),
+                            "tiff" | "tif" => (IMAGE, "tiff"),
+                            "webp" => (IMAGE, "webp"),
+                            // font
+                            "otf" => (FONT, "otf"),
+                            "ttf" => (FONT, "ttf"),
+                            "woff" => (FONT, "woff"),
+                            "woff2" => (FONT, "woff2"),
+                            // application
+                            "js" => (APPLICATION, "javascript"),
+                            "wasm" => (APPLICATION, "wasm"),
+                            "xml" => (APPLICATION, "xml"),
+                            "pdf" => (APPLICATION, "pdf"),
+                            _ => (APPLICATION, "octet-stream"),
+                        };
+
+                        let data = fs::read(entry.path()).await?;
+
+                        let sample_data_len = data.len().min(1024);
+                        let mut gzip_ratio_buf = vec![];
+                        flate2::read::GzEncoder::new(
+                            &data[..sample_data_len],
+                            flate2::Compression::best(),
+                        )
+                        .read_to_end(&mut gzip_ratio_buf)?;
+
+                        let gzip_ratio = gzip_ratio_buf.len() as f32 / sample_data_len as f32;
+
+                        // Check if the gzip ratio is good enough to justify the additional
+                        // computation.
+                        let (variation, data) = if gzip_ratio < 0.9 {
+                            let mut gzip_data = vec![];
+                            flate2::read::GzEncoder::new(&data[..], flate2::Compression::best())
+                                .read_to_end(&mut gzip_data)?;
+
+                            (
+                                FileMetadataVariation {
+                                    len: gzip_data.len() as u64,
+                                    headers: vec![(
+                                        "content-encoding".to_string(),
+                                        "gzip".to_string(),
+                                    )],
+                                },
+                                gzip_data,
+                            )
+                        } else {
+                            (
+                                FileMetadataVariation {
+                                    len: data.len() as u64,
+                                    headers: vec![],
+                                },
+                                data,
+                            )
+                        };
+
+                        let metadata = FileMetadata {
+                            headers: vec![(
+                                "content-type".to_owned(),
+                                format!("{}/{}", content_type.0, content_type.1),
+                            )],
+                            variations: vec![variation],
+                        };
+
+                        let mut data_with_header = vec![1];
+
+                        let bincode_options = bincode::options().allow_trailing_bytes();
+                        bincode_options.serialize_into(&mut data_with_header, &metadata)?;
+
+                        data_with_header.extend_from_slice(&data);
+
+                        let hash = blake3::hash(&data_with_header);
+
+                        let mut is_directory = false;
 
                         if relative_path.ends_with("index.html")
                             || relative_path.ends_with("index.htm")
                         {
                             relative_path = relative_path.parent().unwrap().to_owned();
+                            is_directory = true;
                         }
 
                         let mut web_path = String::new();
@@ -127,14 +227,21 @@ async fn main() -> Result<()> {
                             web_path.push_str(part.to_str().unwrap());
                         }
 
-                        if web_path.is_empty() {
+                        if web_path.is_empty() || is_directory {
                             web_path.push_str("/");
                         }
 
-                        found_files.push((web_path, entry.path(), hash.to_hex().to_string()));
+                        found_files.push((
+                            web_path,
+                            entry.path(),
+                            hash.to_hex().to_string(),
+                            data_with_header,
+                        ));
                     }
                 }
             }
+
+            println!("Found {} files to upload.", found_files.len());
 
             let client = reqwest::Client::new();
 
@@ -156,7 +263,7 @@ async fn main() -> Result<()> {
                     site_name: site_name.clone(),
                     files: found_files
                         .iter()
-                        .map(|(path, _, hash)| CreateDeploymentFiles {
+                        .map(|(path, _, hash, _)| CreateDeploymentFiles {
                             path: path.clone(),
                             hash: hash.clone(),
                         })
@@ -168,24 +275,42 @@ async fn main() -> Result<()> {
                 .json::<Deployment>()
                 .await?;
 
+            let upload_limit = Arc::new(tokio::sync::Semaphore::new(4));
+            let mut upload_tasks = vec![];
+
             for file_to_upload in deployment.files_to_upload {
-                let (_, fs_path, _) = found_files
+                let (_, _, _, data_with_header) = found_files
                     .iter()
-                    .find(|(path, _, _)| path == &file_to_upload)
+                    .find(|(path, _, _, _)| path == &file_to_upload)
                     .unwrap();
 
-                client
+                let upload_limit = upload_limit.clone();
+
+                let res = client
                     .put(format!("{remote}/api/v1/files"))
                     .query(&CreateFileQuery {
                         site_name: site_name.clone(),
                         deployment_id: deployment.id,
                         path: file_to_upload,
                     })
-                    .body(fs::read(&fs_path).await.unwrap())
-                    .send()
-                    .await?
-                    .error_for_status()
-                    .unwrap();
+                    .body(data_with_header.clone())
+                    .send();
+
+                let task = tokio::spawn(async move {
+                    let upload_permit = upload_limit.acquire().await.unwrap();
+
+                    let res = res.await;
+
+                    drop(upload_permit);
+
+                    res
+                });
+
+                upload_tasks.push(task);
+            }
+
+            for upload_task in upload_tasks {
+                upload_task.await??;
             }
 
             let update_site = if stage {
@@ -242,6 +367,18 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct FileMetadata {
+    headers: Vec<(String, String)>,
+    variations: Vec<FileMetadataVariation>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct FileMetadataVariation {
+    len: u64,
+    headers: Vec<(String, String)>,
+}
+
 #[derive(Debug, Deserialize)]
 struct GetFilePath {
     site_name: String,
@@ -256,6 +393,7 @@ async fn get_file(
     Path(path): Path<GetFilePath>,
 ) -> impl IntoResponse {
     let mut headers = HeaderMap::new();
+    headers.insert(header::X_CONTENT_TYPE_OPTIONS, "nosniff".parse().unwrap());
 
     let mut full_path = String::with_capacity(path.path.len() + 1);
     full_path.push_str("/");
@@ -323,20 +461,38 @@ async fn get_file(
         .child(&path_hash_hex[..2])
         .child(&path_hash_hex[2..]);
 
-    let (_, ext) = path.path.rsplit_once(".").unwrap_or(("", ""));
-
-    let mime = match ext {
-        "html" | "htm" | "" => "text/html",
-        "jpeg" | "jpg" => "image/jpeg",
-        _ => "application/octet-stream",
-    };
-
-    headers.insert(header::CONTENT_TYPE, mime.parse().unwrap());
-
     let file_obj = state.object_store.get(&file_path).await.unwrap();
     let file_raw = file_obj.bytes().await.unwrap().to_vec();
+    let mut file_cursor = Cursor::new(&file_raw[1..]);
 
-    (StatusCode::OK, headers, file_raw)
+    let file_header: FileMetadata = bincode::options()
+        .allow_trailing_bytes()
+        .deserialize_from(&mut file_cursor)
+        .unwrap();
+
+    let header_length = 1 + file_cursor.position() as usize;
+
+    for (header_name, header_value) in file_header.headers.iter() {
+        headers.insert(
+            HeaderName::try_from(header_name).unwrap(),
+            header_value.parse().unwrap(),
+        );
+    }
+
+    let variation = file_header.variations.first().unwrap();
+
+    for (header_name, header_value) in variation.headers.iter() {
+        headers.insert(
+            HeaderName::try_from(header_name).unwrap(),
+            header_value.parse().unwrap(),
+        );
+    }
+
+    (
+        StatusCode::OK,
+        headers,
+        file_raw[header_length..header_length + variation.len as usize].to_owned(),
+    )
 }
 
 async fn create_deployment(
